cc8453cd50fdf9946ef9c738a5223804e18f74ec
Omar <ojimenez-ecw@cricut.com>
2026-08-25 20:57:53 +0000

    chore: initialize project tooling
    Root npm workspace, shared TypeScript config, ESLint flat config + Prettier,
    editorconfig/nvmrc, and Husky + lint-staged for pre-commit checks.

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/.vite/**', '**/generated/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['packages/client/**/*.{ts,tsx}'],
+    plugins: { 'react-hooks': reactHooks },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+  eslintConfigPrettier,
+);
